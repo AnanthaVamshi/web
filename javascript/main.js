4028cfce0681d87610b47876bc701b5445053d44
AnanthaVamshi <AnanthaVamshi@github.com>
2021-05-03 22:56:31 +0530

    first

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+const name = 'vamshi';
+const age = 30;
+const isCool = true;
+const x = null;
+const rating = 4.7;
+const y = undefined;
+let z;
+
+const fruits = ["apple", "berry"]
+
+fruits.push("banana")
+
+fruits[5] = "cherry"
+
+const person = {
+  firstname : "vamshi",
+  lastname : "krishna",
+  age : 30,
+  Hobbies : ["playing", "swimming", "tv", "movies"],
+  address : {
+    city: "hyd",
+    street : "ra",
+    state : "tg"
+ }
+}
+
+console.log(person.address.state)
+console.log(window)
